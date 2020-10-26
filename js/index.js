0cadaeddc157674733a6d768e3a0036803c08f61
Yooninsun <jein8954@naver.com>
2020-10-26 14:53:40 +0900

    24시동물병원 list추가

--- js/index.js
+++ js/index.js
@@ -39,22 +39,6 @@ $.ajax({
   }
 })
 
-var usedata2;
-$.ajax({
-  type : 'GET',
-  url : 'data/allHours.json', 
-  beforeSend : function(xhr) {
-    if (xhr.ovrrideMimeType) {
-      xhr.overrideMimetype("application/json")
-    }
-  },
-  success : function(data2) {
-    usedata2 = data2;
-  },
-  error : function(error) {
-    alert(error.status + '오류발생')
-  }
-})
 
 /* 전체병원 list 클릭시 medicalCenter json파일 불러오기 */
 $('body').on('click', '.mainContent .hospital a, #lnb_navi a', function(e) {
@@ -85,14 +69,14 @@ $('body').on('click', '.mainContent .allDay a, #lnb_navi a', function(e) {
   $('#container > #content').remove();
   $('#container').load(url + ' #content', function() {
     var newContent = '';
-    for(var i in usedata2[part]) {
-      newContent += `<li><div class="allHoursInfo col-12 col-md-12"><div class="img"><img src="${usedata2[part][i].img}" alt="동물병원사진"></div>`; 
-      newContent += `<h4>${usedata2[part][i].name}</h4>`;
-      newContent += `<a href="tel:${usedata2[part][i].tell}">${usedata2[part][i].tell}</a>`;
-      newContent += `<p class="add">${usedata2[part][i].address}</p>`;
-      newContent += `<p class="hour1">${usedata2[part][i].hours1}</p>`;
-      newContent += `<p class="hour2">${usedata2[part][i].hours2}</p>`;
-      newContent += `<p class="homepage"><a href="${usedata2[part][i].hompage}">${usedata2[part][i].hompage}</a></p></div></li>`
+    for(var i in usedata[part]) {
+      newContent += `<li><div class="allHoursInfo col-12 col-md-12"><div class="img"><img src="${usedata[part][i].img}" alt="동물병원사진"></div>`; 
+      newContent += `<h4>${usedata[part][i].name}</h4>`;
+      newContent += `<a href="tel:${usedata[part][i].tell}">${usedata[part][i].tell}</a>`;
+      newContent += `<p class="add">${usedata[part][i].address}</p>`;
+      newContent += `<p class="hour1">${usedata[part][i].hours1}</p>`;
+      newContent += `<p class="hour2">${usedata[part][i].hours2}</p>`;
+      newContent += `<p class="homepage"><a href="${usedata[part][i].hompage}">${usedata[part][i].hompage}</a></p></div></li>`
     }
     $('#content .allHoursList').html(`<ul>${newContent}</ul>`);
   })
